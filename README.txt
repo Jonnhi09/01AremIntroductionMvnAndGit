ca7a1f4f8e6bb20ff9b7b8e4f88381e4578981dc
jonnhi09 <jonnhi.prietou@gmail.com>
2018-08-16 23:57:41 -0500

    Operaciones creadas y actualizaciones

--- README.txt
+++ README.txt
@@ -1,1 +1,20 @@
-# Taller de introducci¢n a Maven y Git
+## Escuela Colombiana de Ingeniería Julio Garavito
+
+### Jonathan Prieto
+### Arquitecturas Empresariales
+
+### Taller de introducci¢n a Maven y Git
+
+---
+
+#### Requirements
+
+- Write a program to calculate the mean and standard deviation of a set of n real  numbers.
+
+- Your program reads the n real numbers from a file.
+
+- Use a linked list to store the n numbers for the calculations.
+
+
+
+Se adjunta el archivo ClassDiagram.asta e IntroductionMvnAndGit.pdf con el modelo y descripción de la aplicación.
--- src/main/java/edu/escuelaing/arem/app/App.java
+++ src/main/java/edu/escuelaing/arem/app/App.java
@@ -1,12 +1,28 @@
 package edu.escuelaing.arem.app;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
  *
  * Jonathan Prieto.
  */
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        String archivo = "C:\\Users\\jonnh\\Documents\\NetBeansProjects\\introductionMvnAndGit\\numbers.txt"; //Ruta del archivo a leer.
+        LinkedList numberList = new LinkedList();
+        Estadistico statistical;
+        String line;
+        FileReader fr = new FileReader(archivo);
+        BufferedReader br = new BufferedReader(fr);
+        while((line=br.readLine())!=null){
+            numberList.addNode(Integer.parseInt(line)); //Agregar nodos a la LinkedList.
+        }
+        br.close();
+        statistical = new Estadistico(numberList);
+        System.out.println("Mean = "+statistical.mean()+"\nStandard deviation = "+statistical.standardDeviation());
     }
 }
